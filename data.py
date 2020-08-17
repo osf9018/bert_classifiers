f11407e28b02e708c1dea92ce1763c703713bb29
Olivier Ferret <olivier.ferret@cea.fr>
2020-08-17 15:42:26 +0200

    use filenames provided through command line arguments for training and test data. Add the possibility to define validation data

--- data.py
+++ data.py
@@ -8,22 +8,19 @@ from transformers import BasicTokenizer
 
 from utils import retokenize
 
-DATA_PATH = 'data/'
 ClassificationExample = namedtuple(
     'ClassificationExample', ['id', 'tokens_a', 'tokens_b', 'label'])
 SequenceLabellingExample = namedtuple(
     'SequenceLabellingExample', ['id', 'token_sequence', 'label_sequence'])
 
 
-def load_classification_dataset(step, do_lower_case):
-    """ Loads classification exampels from a dataset. """
-    assert step in ['train', 'test']
+def load_classification_dataset(data_filename, do_lower_case):
+    """ Loads classification examples from a dataset. """
     basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case)
-    path = os.path.join(DATA_PATH, 'classification', f'{step}.txt')
     examples = []
-    with open(path, 'r', encoding='utf-8') as data_file:
+    with open(data_filename, 'r', encoding='utf-8') as data_file:
         lines = data_file.readlines()
-        for i, line in tqdm(enumerate(lines), desc=f'reading `{os.path.basename(path)}`...'):
+        for i, line in tqdm(enumerate(lines), desc=f'reading `{os.path.basename(data_filename)}`...'):
             # example: __label__negative I don't like tomatoes.
             splitline = line.strip().split()
             label = splitline[0].split('__label__')[-1]
@@ -36,20 +33,18 @@ def load_classification_dataset(step, do_lower_case):
                     label=label,
                 )
             )
-    logging.info('Number of `%s` examples: %d', step, len(examples))
+    logging.info('Number of `%s` examples: %d', data_filename, len(examples))
     return examples
 
-def load_sequence_labelling_dataset(step, do_lower_case):
+def load_sequence_labelling_dataset(data_filename, do_lower_case):
     """ Loads sequence labelling examples from a dataset. """
-    assert step in ['train', 'test']
-    path = os.path.join(DATA_PATH, 'sequence_labelling', f'{step}.txt')
     i = 0
     examples = []
-    with open(path, 'r', encoding='utf-8') as data_file:
+    with open(data_filename, 'r', encoding='utf-8') as data_file:
         lines = data_file.readlines()
         token_sequence = []
         label_sequence = []
-        for line in tqdm(lines, desc=f'reading `{os.path.basename(path)}`...'):
+        for line in tqdm(lines, desc=f'reading `{os.path.basename(data_filename)}`...'):
             # example:
             #          My O
             #          name O
@@ -62,14 +57,15 @@ def load_sequence_labelling_dataset(step, do_lower_case):
                 token_sequence.append(token)
                 label_sequence.append(label)
             else:
-                examples.append(
-                    SequenceLabellingExample(
-                        id=i,
-                        token_sequence=token_sequence,
-                        label_sequence=label_sequence,
+                if token_sequence:
+                    examples.append(
+                        SequenceLabellingExample(
+                            id=i,
+                            token_sequence=token_sequence,
+                            label_sequence=label_sequence,
+                        )
                     )
-                )
-                i += 1
+                    i += 1
                 token_sequence = []
                 label_sequence = []
 
@@ -82,9 +78,8 @@ def load_sequence_labelling_dataset(step, do_lower_case):
                 label_sequence=label_sequence,
             )
         )
-
     retokenize(
         examples,
         tokenization_function=BasicTokenizer(do_lower_case=do_lower_case).tokenize)
-    logging.info('Number of `%s` examples: %d', step, len(examples))
+    logging.info('Number of `%s` examples: %d', data_filename, len(examples))
     return examples
