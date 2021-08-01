423006895a097a17b8ff665b8d8dfa8ce9855b0f
Olivier Ferret <olivier.ferret@cea.fr>
2021-08-01 18:54:30 +0200

    initial version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bert_classifiers',
+      version='0.1',
+      description='code for training and testing NLP tools based on BERT',
+      url='http://github.com/osf9018/bert_classifiers',
+      author='osf9018',
+      author_email='osf9018@example.com',
+      license='Apache License 2.0',
+      packages=['bert_classifiers'],
+      zip_safe=False)
